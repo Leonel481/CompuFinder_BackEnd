b53f4e64d847c1d12590f9351a36d3270cf1c736
Leonel481 <leonel.aliaga.v@gmail.com>
2025-01-22 22:09:17 -0500

    Cambios en configuracion, nginx, cors de la api habilitado modo cookie

--- settings/dev.py
+++ settings/dev.py
@@ -7,6 +7,13 @@ DEBUG = env.bool('DJANGO_DEBUG', default=True)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
+CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=[])
+
+CORS_ALLOW_METHODS = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT']
+CORS_ALLOW_HEADERS = ['accept', 'accept-encoding', 'authorization', 'content-type', 'dnt', 'origin', 'user-agent', 'x-csrftoken', 'x-requested-with']
+
+CORS_ALLOW_CREDENTIALS = True
+
 DATABASES = {
    "default": {
        "ENGINE": "django.db.backends.postgresql",
